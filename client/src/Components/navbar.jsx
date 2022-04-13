dc4ad642705ee35810c70862be3a10386a6e2f5c
mayank190821 <bhugramayank@gmail.com>
2022-04-13 12:10:09 +0530

    added navbar component

---
+++ client/src/Components/navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Toolbar } from "@mui/material";
+
+export default function NavBar(){
+    return(
+        <Box sx={{flexGrow:1}} >
+            <AppBar style={{ backgroundColor:"#0d4783"}}>
+                <Toolbar>
+
+                <Typography variant="h5" sx= {{flexGrow:1}}>
+                    ResumeMaker
+                </Typography>
+                <Button color="inherit" variant="h5"> LogIn </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
