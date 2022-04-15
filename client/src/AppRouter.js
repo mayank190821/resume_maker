572b5a9db0ead95b21cf32f8db0ea704849b94e3
rupin-1801 <rupinbhugra1411@gmail.com>
2022-04-15 13:02:43 +0530

    used context in navbar

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,14 +1,27 @@
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import Navbar from "./Components/navbar";
 import Login from "./pages/login";
+import {useState} from "react";
+import {Dialog} from "@mui/material";
+import {OpenLoginContext} from "./context.js";
 
 function AppRouter() {
+  const [open, setOpen] = useState(false);
+  
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar/>}/>
-        <Route path="/login" element={<Login/>}/>
-      </Routes>
+      <OpenLoginContext.Provider value={{openLogin: open, setOpenLogin: setOpen}}>
+        <Routes>
+          <Route path="/" element={<Navbar/>}/>
+        </Routes>
+        <Dialog
+          open={open}
+          keepMounted
+          onClose={() => {setOpen(false)}}
+          >
+          <Login setOpen={setOpen}/>
+        </Dialog>
+      </OpenLoginContext.Provider>
     </BrowserRouter>
   );
 }
--- client/src/Components/navbar.jsx
+++ client/src/Components/navbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Toolbar } from "@mui/material";
+import {OpenLoginContext} from "./../context.js";
 
 export default function NavBar(){
+    const {setOpenLogin} = useContext(OpenLoginContext);
     return(
         <Box sx={{flexGrow:1}} >
             <AppBar style={{ backgroundColor:"#0d4783"}}>
@@ -14,7 +16,7 @@ export default function NavBar(){
                 <Typography variant="h5" sx= {{flexGrow:1}}>
                     ResumeMaker
                 </Typography>
-                <Button color="inherit" variant="h5"> LogIn </Button>
+                <Button onClick={() => {setOpenLogin(true)}} color="inherit" variant="h5"> LogIn </Button>
                 </Toolbar>
             </AppBar>
         </Box>
