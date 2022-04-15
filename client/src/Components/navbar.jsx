230f0e4153f0f22802c9ef85e0b9133403ef806c
mayank190821 <bhugramayank@gmail.com>
2022-04-15 13:07:14 +0530

    added google fonts in landing page, changed color in navbar and added button background color

--- client/src/Components/navbar.jsx
+++ client/src/Components/navbar.jsx
@@ -8,12 +8,12 @@ import { Toolbar } from "@mui/material";
 export default function NavBar(){
     return(
         <Box>
-            <AppBar position="relative" style={{backgroundColor:"white"}}>
-                <Toolbar style={{color:"#0d4783"}}>
-                <Typography variant="h5" sx= {{flexGrow:1}}>
+            <AppBar position="relative" style={{backgroundColor:"white", padding:"0 50px"}}>
+                <Toolbar style={{color:"#006370"}}>
+                <Typography variant="h5" sx= {{flexGrow:1}} style={{fontWeight:"500",letterSpacing:"2px",fontSize:"35px",fontFamily:"Beau Rivage, cursive"}}>
                     ResumeMaker
                 </Typography>
-                <Button color="#0d4783" variant="h5"> LogIn </Button>
+                <Button style={{backgroundColor:"#006370",color:"white"}} variant="h5"> LogIn </Button>
                 </Toolbar>
             </AppBar>
         </Box>
